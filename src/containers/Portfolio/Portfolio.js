4e4ec35c1ab09255479b458d1736d617b114275c
andrijaDomazet <domazet.andrija@gmail.com>
2019-10-30 15:44:42 +0100

    Working on Portfolio page

--- src/containers/Portfolio/Portfolio.js
+++ src/containers/Portfolio/Portfolio.js
@@ -1,5 +1,25 @@
 import React from "react";
+import data from "../../Deatils";
+import "./Portfolio.scss";
 
 export default function Portfolio() {
-  return <div>Portfolio</div>;
+  console.log(data);
+
+  return (
+    <div className="portfolio">
+      <h1>Portfolio</h1>
+      <div className="papers">{papers()}</div>
+    </div>
+  );
 }
+
+const papers = () => {
+  return data.map((paper, index) => {
+    return (
+      <div key={index} className="paper">
+        <h5>{paper.title}</h5>
+        <p>{paper.body}</p>
+      </div>
+    );
+  });
+};
